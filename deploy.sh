80cc795db996cc9988e25bc07485b090a7001a54
jaybenaim <benaimjacob@gmail.com>
2020-08-27 14:30:21 -0400

    fixed deploy.sh push to correct repo name

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:jaybenaim/jaybenaim.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jaybenaim/jaybenaim.git master:gh-pages
+git push -f git@github.com:jaybenaim/vue-demo.git master:gh-pages
 
 cd -

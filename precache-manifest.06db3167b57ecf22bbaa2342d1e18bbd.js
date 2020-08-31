5b2c5d9ad73ff401ba4d0d1f59ec5dc0021f7885
jaybenaim <benaimjacob@gmail.com>
2020-08-31 17:43:09 -0400

    deploy

---
+++ precache-manifest.06db3167b57ecf22bbaa2342d1e18bbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaf42381ec88ba05599",
+    "url": "/vue-demo/css/app.9a8547dc.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "65d25bef846679ed469262e186ad741b",
+    "url": "/vue-demo/index.html"
+  },
+  {
+    "revision": "94a78bff5269ccf228d9",
+    "url": "/vue-demo/js/about.fe3d965a.js"
+  },
+  {
+    "revision": "daaf42381ec88ba05599",
+    "url": "/vue-demo/js/app.b500280c.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/vue-demo/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "df5632bc93f20fe2ea2d6de97b55fbe9",
+    "url": "/vue-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-demo/robots.txt"
+  }
+]);
